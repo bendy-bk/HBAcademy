56032dd5fd3998ea56b9ee40fc553708241053a7
HiepTran <hiep61027@gmail.com>
2025-04-14 20:13:01 +0700

    bt1
    bubble sort arr

---
+++ Assets/bt1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bt1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] input = { 1, 3, 4, 5, 2, 4, 6, 9 };
+        DisplayArray(input);
+        BubbleSort(input);
+        DisplayArray(input);
+    }
+
+    public void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void DisplayArray(int[] array)
+    {
+        foreach (var i in array)
+        {
+            Debug.Log(i + ",");
+        }
+    }
+
+
+}
