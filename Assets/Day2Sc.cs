27dba67c7b32b024eeaaf681c60bddaa55c9316c
hieptran <hiep61027@gmail.com>
2025-04-16 20:41:03 +0700

    finalCommit

---
+++ Assets/Day2Sc.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day2Sc : Monobe
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void TurnOnFire()
+    {
+        
+    }
+
+}
+/////
+//    1A
+//    2A
+//    3A
+//    4B
+//    5D
+//    6D
+//    7C
+//    8A
+//    9D
+//    10C
+//    11B
+//    12B
+//    13A
+//    14C
+//    15D
+//    16B
+//    17D
+//    18D
---
+++ Assets/Script/Day2Sc.cs
@@ -0,0 +1,43 @@
+using Unity.Engine;
+
+public class Day2Sc : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void TurnOnFire()
+    {
+        
+    }
+
+}
+/////
+//    1A
+//    2A
+//    3A
+//    4B
+//    5D
+//    6D
+//    7C
+//    8A
+//    9D
+//    10C
+//    11B
+//    12B
+//    13A
+//    14C
+//    15D
+//    16B
+//    17D
+//    18D
